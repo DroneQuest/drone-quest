840caa590008adc0f61a17ba71411f8bf3fd886e
Norton Pengra <npengra317@gmail.com>
2016-05-02 10:34:04 -0700

    New client for testing server

---
+++ tests/client.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+import socket
+
+
+PORT = 3000
+BUFFER_LENGTH = 1024
+
+
+def setup_socket():
+    """Gather required information for building a socket object"""
+    info = socket.getaddrinfo('127.0.0.1', PORT)
+    return [i for i in info if i[1] == socket.SOCK_STREAM][0]
+
+
+def build_client(socket_details):
+    """Use given information to build a socket object"""
+    client = socket.socket(socket.AF_INET,
+                           socket.SOCK_STREAM,
+                           socket.IPPROTO_TCP)
+    client.settimeout(1)
+    return client
+
+
+def close(socket):
+    """Close socket gracefully"""
+    socket.close()
+
+
+def send_message(socket, message):
+    """Send a scrubbed message to the server"""
+    socket.connect(('127.0.0.1', PORT))
+    socket.sendall(message)
+
+
+def get_reply(client):
+    """Get reply from the server"""
+    chunks = []
+    while True:
+        chunk = client.recv(BUFFER_LENGTH)
+        chunks.append(chunk)
+        if len(chunk) < BUFFER_LENGTH or len(chunk) == 0:
+            return (b''.join(chunks)).decode('utf-8').replace('\r', '')
+
+
+# if __name__ == "__main__":
+    # client = build_client(setup_socket())
+    # close(client)
