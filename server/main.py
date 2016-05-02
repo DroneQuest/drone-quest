0cadab073ea67d0193f8f74319c92d2562e526fa
muniri92 <mibrah04@gmail.com>
2016-05-02 11:45:30 -0700

    changed the IP address that the clients connects to

--- server/main.py
+++ server/main.py
@@ -1,10 +1,12 @@
 # -*-coding:utf-8-*-
-"""Handle server operations of reading incoming streams and echoing them"""
+"""Handle server operations of reading incoming streams and echoing them."""
 import socket
+from venthur_api import libardrone
 
-buffer_length = 1024
+DRONE = libardrone.ARDrone()
+BUFFER_LENGTH = 1024
 PORT = 3000
-IP = "0.0.0.0"
+IP = "127.0.0.1"
 
 PING = 0x00
 TAKE_OFF = 0x01
@@ -20,12 +22,33 @@ TURN_LEFT = 0x0A
 TURN_RIGHT = 0x0B
 RESET = 0x0C
 CALIBRATE = 0x0D
-SPEED = 0x0E
+INCREASE_SPEED = 0x0E
+DECREASE_SPEED = 0x0F
 TERMINATE = 0x10
+
+COMMANDS_CENTRAL = {
+    PING: DRONE.ping,
+    TAKE_OFF: DRONE.takeoff,
+    LAND: DRONE.land,
+    HOVER: DRONE.hover,
+    MOVE_LEFT: DRONE.move_left,
+    MOVE_RIGHT: DRONE.move_right,
+    MOVE_UP: DRONE.move_up,
+    MOVE_DOWN: DRONE.move_down,
+    MOVE_FORWARD: DRONE.move_forward,
+    MOVE_BACKWARD: DRONE.move_backward,
+    TURN_LEFT: DRONE.turn_left,
+    TURN_RIGHT: DRONE.turn_right,
+    RESET: DRONE.reset,
+    CALIBRATE: DRONE.trim,
+    INCREASE_SPEED: DRONE.increase_speed,
+    DECREASE_SPEED: DRONE.decrease_speed,
+    TERMINATE: DRONE.halt
+}
 
 
 def setup_server():
-    """Build a socket object on localhost and specified port"""
+    """Build a socket object on localhost and specified port."""
     server = socket.socket(socket.AF_INET,
                            socket.SOCK_STREAM,
                            socket.IPPROTO_TCP)
@@ -35,29 +58,34 @@ def setup_server():
 
 
 def server_listen(server):
-    """Accept connections from the client"""
+    """Accept connections from the client."""
     conn, addr = server.accept()
     return (conn, addr)
 
 
 def server_read(connection):
-    """Read and parse message from client"""
-
+    """Read and parse message from client."""
     string = ''.encode('utf-8')
     while True:
-        part = connection.recv(buffer_length)
+        part = connection.recv(BUFFER_LENGTH)
         string += part
-        if len(part) < buffer_length or len(part) == 0:
+        if len(part) < BUFFER_LENGTH or len(part) == 0:
             break
     return string.decode('utf-8')
 
 
 def server_response(string, connection):
-    """Send back specified string to specified connection"""
+    """Send back specified string to specified connection."""
     if isinstance(string, bytes):
         connection.send(string)
     else:
         connection.send(string.encode('utf-8'))
+
+
+def parse_command(command):
+    """Convert the command to hexadecimals."""
+    command = hex(int(command, base=16))
+    DRONE[command]()
 
 
 def server():
@@ -68,6 +96,7 @@ def server():
             connection, address = socket.accept()
             command = server_read(connection)
             print("recv:", command)
+            server_response(b'OK', connection)
             connection.close()
     except KeyboardInterrupt:
         print("Closing the server!")
