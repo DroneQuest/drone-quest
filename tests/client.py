a297520d87870f9d9cd03e713611df76bec429de
muniri92 <mibrah04@gmail.com>
2016-05-02 11:56:30 -0700

    running first test

--- tests/client.py
+++ tests/client.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+"""Dummy Test Client."""
 import socket
 
 
@@ -7,13 +8,13 @@ BUFFER_LENGTH = 1024
 
 
 def setup_socket():
-    """Gather required information for building a socket object"""
+    """Gather required information for building a socket object."""
     info = socket.getaddrinfo('127.0.0.1', PORT)
     return [i for i in info if i[1] == socket.SOCK_STREAM][0]
 
 
 def build_client(socket_details):
-    """Use given information to build a socket object"""
+    """Use given information to build a socket object."""
     client = socket.socket(socket.AF_INET,
                            socket.SOCK_STREAM,
                            socket.IPPROTO_TCP)
@@ -22,18 +23,18 @@ def build_client(socket_details):
 
 
 def close(socket):
-    """Close socket gracefully"""
+    """Close socket gracefully."""
     socket.close()
 
 
 def send_message(socket, message):
-    """Send a scrubbed message to the server"""
+    """Send a scrubbed message to the server."""
     socket.connect(('127.0.0.1', PORT))
     socket.sendall(message)
 
 
 def get_reply(client):
-    """Get reply from the server"""
+    """Get reply from the server."""
     chunks = []
     while True:
         chunk = client.recv(BUFFER_LENGTH)
--- tests/test_server.py
+++ tests/test_server.py
@@ -1,10 +1,18 @@
 # -*- coding: utf-8 -*-
+"""Test the drone server."""
 import pytest
 from tests import client as client_module
 
-@pytest.fixture(scope='module')
+
+@pytest.fixture(scope='function')
 def client():
+    """Client fixture."""
     return client_module.build_client(client_module.setup_socket())
 
 
-client_module.close(client)
+def test_ping(client):
+    """Test that the client can send a message that we interpet as a ping."""
+    client_module.send_message(client, '0x00')
+    resp = client_module.get_reply(client)
+    assert resp == b'OK'
+    client_module.close(client)
