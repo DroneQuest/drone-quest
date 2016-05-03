cba104e5e28682002a7d36754c8a4470d6a242e6
Will Weatherford <weatherford.william@gmail.com>
2016-05-02 19:30:36 -0700

    Attempting to open and serve index.html at route base.

--- venthur_api/api.py
+++ venthur_api/api.py
@@ -1,11 +1,20 @@
 """Set up a bottle server to accept post requests commanding the drone."""
-from bottle import post, run
+from bottle import post, run, route
 import libardrone
+import os
 
 drone = libardrone.ARDrone()
 
+HTML_LOC = '/home/will/401d2/drone-client/dist'
 
-@post('/do/<command>')
+
+@route('/')
+def home():
+    with open(os.path.join(HTML_LOC, 'index.html')) as html:
+        return html
+
+
+@route('/do/<command>')
 def do(command):
     """Execute the given command from the route."""
     try:
