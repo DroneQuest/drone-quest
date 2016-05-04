72d9a5a42a912ecf4bc219c9b9a5c8e773da74a2
Norton Pengra <npengra317@gmail.com>
2016-05-03 19:01:11 -0700

    Updated tests

--- tests/test_server.py
+++ tests/test_server.py
@@ -1,22 +1,22 @@
 # -*- coding: utf-8 -*-
 """Test the drone server."""
 import pytest
-from server import client as client_module
+from requests import post
 
+NET_LOC = "http://127.0.0.1:3000/do/"
 
-COMMAND_TESTS_PASS = [(hex(message), "OK") for message in range(16)]
+VALID_COMMANDS = (
+    "move_left move_right move_forward move_backward move_up move_down "
+    "turn_left turn_right hover takeoff land"
+).split()
 
-
-@pytest.fixture(scope='function')
-def client():
-    """Client fixture."""
-    return client_module.build_client(client_module.setup_socket())
+COMMAND_TESTS_PASS = [(message, "Command executed: {}".format(message)) for message in VALID_COMMANDS]
 
 
 @pytest.mark.parametrize("message,expected_response", COMMAND_TESTS_PASS)
-def test_command(client, message, expected_response):
+def test_command(message, expected_response):
     """Test the server responds to all commands correctly."""
-    client_module.send_message(client, message)
-    resp = client_module.get_reply(client)
-    assert resp == expected_response
-    client_module.close(client)
+    location = NET_LOC + message
+    # import pdb; pdb.set_trace()
+
+    assert post(location).text == expected_response
