21b71e52d5e2e4c3042366a4d1b22c203656449e
Norton Pengra <npengra317@gmail.com>
2016-05-03 19:04:11 -0700

    New hit on test_navdata

--- tests/test_server.py
+++ tests/test_server.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 """Test the drone server."""
 import pytest
-from requests import post
+from requests import post, get
 
-NET_LOC = "http://127.0.0.1:3000/do/"
+NET_LOC = "http://127.0.0.1:3000/"
 
 VALID_COMMANDS = (
     "move_left move_right move_forward move_backward move_up move_down "
@@ -16,7 +16,12 @@ COMMAND_TESTS_PASS = [(message, "Command executed: {}".format(message)) for mess
 @pytest.mark.parametrize("message,expected_response", COMMAND_TESTS_PASS)
 def test_command(message, expected_response):
     """Test the server responds to all commands correctly."""
-    location = NET_LOC + message
+    location = NET_LOC + "do/" + message
     # import pdb; pdb.set_trace()
 
     assert post(location).text == expected_response
+
+
+def test_navdata():
+    location = NET_LOC + 'navdata'
+    assert get(location).ok
