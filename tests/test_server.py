a91c64ea20b1a6d7125299aff262cb799f870dfe
Norton Pengra <npengra317@gmail.com>
2016-05-03 19:49:24 -0700

    Updated tests to no longer require active server
    No longer need requests library to just test the server!

--- tests/test_server.py
+++ tests/test_server.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """Test the drone server."""
 import pytest
-from requests import post, get
+from server.bottle_drone import navdata, do
 
 NET_LOC = "http://127.0.0.1:3000/"
 
@@ -14,14 +14,11 @@ COMMAND_TESTS_PASS = [(message, "Command executed: {}".format(message)) for mess
 
 
 @pytest.mark.parametrize("message,expected_response", COMMAND_TESTS_PASS)
-def test_command(message, expected_response):
-    """Test the server responds to all commands correctly."""
-    location = NET_LOC + "do/" + message
-    # import pdb; pdb.set_trace()
-
-    assert post(location).text == expected_response
+def test_functional_command(drone, message, expected_response):
+    response = do(message, drone=drone)
+    assert response == expected_response
 
 
-def test_navdata():
-    location = NET_LOC + 'navdata'
-    assert get(location).ok
+def test_nav_data(drone):
+    response = navdata(drone=drone)
+    assert response == drone.navdata
