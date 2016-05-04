632a630e6499d1bc0b6f2f4a7ee82111e4581314
Norton Pengra <npengra317@gmail.com>
2016-05-03 19:49:00 -0700

    Created fixtures for mock objects

---
+++ tests/conftest.py
@@ -0,0 +1,23 @@
+import pytest
+from json import dumps
+
+
+class MockDrone(object):
+    # lambdas for brevity
+    navdata = dumps({"some": "json"})
+    move_left = lambda self: None
+    move_right = lambda self: None
+    move_forward = lambda self: None
+    move_backward = lambda self: None
+    move_up = lambda self: None
+    move_down = lambda self: None
+    turn_left = lambda self: None
+    turn_right = lambda self: None
+    hover = lambda self: None
+    takeoff = lambda self: None
+    land = lambda self: None
+
+
+@pytest.fixture()
+def drone():
+    return MockDrone()
