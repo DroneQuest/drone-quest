d85d1562facf9b077fa74e518e8ded10353c180e
Norton Pengra <npengra317@gmail.com>
2016-05-04 11:52:59 -0700

    Testing move_backwards

--- tests/test_leap.py
+++ tests/test_leap.py
@@ -194,6 +194,40 @@ def test_move_forward_hand_closed(controller, drone_listener, position_z):
     drone_listener.on_frame(controller)
     assert history == []
 
+
+@pytest.mark.parametrize("position_z", [px for px in range(50, 100, 10)])
+def test_move_backward(controller, drone_listener, position_z):
+    history = []
+    drone_listener._talk_to_drone = lambda route: history.append(route.split('/')[-1])
+    drone_listener.flying = True
+    controller._test_only_set_palm_position(0, 0, position_z)
+    controller._test_only_set_grab_strength(0)
+    drone_listener.on_frame(controller)
+    assert history == ['move_backward']
+
+
+@pytest.mark.parametrize("position_z", [px for px in range(10, 45, 15)])
+def test_do_not_move_backward(controller, drone_listener, position_z):
+    history = []
+    drone_listener._talk_to_drone = lambda route: history.append(route.split('/')[-1])
+    drone_listener.flying = True
+    controller._test_only_set_palm_position(0, 0, position_z)
+    controller._test_only_set_grab_strength(0)
+    drone_listener.on_frame(controller)
+    assert history == []
+
+
+@pytest.mark.parametrize("position_z", [px for px in range(50, 100, 10)])
+def test_move_backward_hand_closed(controller, drone_listener, position_z):
+    history = []
+    drone_listener._talk_to_drone = lambda route: history.append(route.split('/')[-1])
+    drone_listener.flying = True
+    controller._test_only_set_palm_position(0, 0, position_z)
+    controller._test_only_set_grab_strength(1)
+    drone_listener.on_frame(controller)
+    assert history == []
+
+
 # @pytest.mark.parametrize("s, vx, vy, vz, px, py, pz, f", LANDING_TESTS)
 # def test_hand_position_for_landing(controller, drone_listener, s, vx, vy, vz, px, py, pz, f):
 # # def test_hand_position(controller, drone_listener, requests):
