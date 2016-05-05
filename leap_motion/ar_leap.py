274898b3a956554cd152d9b7c1a98378eda8d1ce
muniri92 <mibrah04@gmail.com>
2016-05-05 12:32:31 -0700

    added docstrings to conftest, and removed all lambdas

--- leap_motion/ar_leap.py
+++ leap_motion/ar_leap.py
@@ -1,8 +1,8 @@
 import time
-# try:
-# import Leap
-# except ImportError:
-from tests.conftest import MockLeap as Leap
+try:
+    from tests.conftest import MockLeap as Leap
+except ImportError:
+    import Leap
 import sys
 import requests
 # from Leap import CircleGesture, KeyTapGesture, ScreenTapGesture, SwipeGesture
@@ -20,18 +20,14 @@ class DroneListener(Leap.Listener):
         """Initialize the leap motion."""
         print("LEAP IS ON")
         self.start_time = time.time()
-        # self.last_flip_time = time.time() - 10
 
     def on_connect(self, controller):
         """Make a connection with the leap."""
         print("LEAP IS CONNECTED")
-
         controller.enable_gesture(Leap.Gesture.TYPE_SWIPE)
+        # Just incase we with to add circular motion
         # controller.enable_gesture(Leap.Gesture.TYPE_CIRCLE)
         controller.enable_gesture(Leap.Gesture.TYPE_KEY_TAP)
-        assert type(Leap.Gesture.TYPE_KEY_TAP) == str
-        # controller.config.set("Gesture.Circle.MinRadius", 100.0)
-
         controller.config.save()
 
     def on_disconnect(self, controller):
@@ -46,7 +42,7 @@ class DroneListener(Leap.Listener):
         if route != self.last:
             self.last = route
             print(route)
-            # return requests.post(route)
+            return requests.post(route)
 
     def _turning_direction(self, hand):
         var = True
@@ -57,36 +53,22 @@ class DroneListener(Leap.Listener):
         var = True
         for i in range(0, 2):
             var = var and hand.fingers[i].is_extended
-        if var and not (hand.fingers[4].is_extended or hand.fingers[3].is_extended):
+        if var and (not hand.fingers[4].is_extended and not hand.fingers[3].is_extended):
+            return 'right'
+        if var and (not hand.fingers[4].is_extended or not hand.fingers[3].is_extended):
             return 'right'
         return 'center'
 
-        # if hand.fingers[0].is_extended is False and all(hand.fingers[0].is_extended is False):
-
     def on_frame(self, controller):
         """Read frames from the drone."""
-        # if (time.time() - self.start_time) < 1.5:
-        # return
-
         frame = controller.frame()
-
-        # for gesture in frame.gestures():
-        #     pass
-        #     # don't have circle yet...
 
         hand = frame.hands[0]
 
-        # if len(hand.fingers) <= 1:
-        #     self.hand_closed(hand)
-        # else:
-        #     self.hand_opened(hand)
-
-        #     if len(hands.fingers) <= 1:
-        # if hand.palm_velocity.y > 100:
         if self.flying is False:
             if hand.grab_strength == 1:
                 if hand.palm_velocity.y >= 1000:
-                    # print("TAKEOFF AND HOVER")
+                    # TAKEOFF DRONE
                     self._talk_to_drone(NET_LOC + "/do/takeoff")
                     self._talk_to_drone(NET_LOC + "/do/hover")
                     self.flying = True
@@ -94,21 +76,21 @@ class DroneListener(Leap.Listener):
         elif self.flying is True:
             if hand.grab_strength == 1:
                 if hand.palm_velocity.y <= -1000:
-                    # print("LAND N STUFF")
+                    # LAND DRONE
                     self._talk_to_drone(NET_LOC + '/do/land')
                     self.flying = False
             elif hand.grab_strength < 0.2:
                 if hand.palm_position.z <= -50:
-                    # print("MOVE FORWARD")
+                    # MOVE DRONE FORWARD
                     self._talk_to_drone(NET_LOC + '/do/move_forward')
                 elif hand.palm_position.z >= 50:
-                    # print("MOVE BACKWARD")
+                    # MOCE DRONE BACKWARD
                     self._talk_to_drone(NET_LOC + '/do/move_backward')
                 elif hand.palm_position.x >= 50:
-                    # print("MOVE RIGHT")
+                    # MOVE DRONE RIGHT
                     self._talk_to_drone(NET_LOC + '/do/move_right')
                 elif hand.palm_position.x <= -50:
-                    # print("MOVE LEFT")
+                    # MOVE DRONE LEFT
                     self._talk_to_drone(NET_LOC + '/do/move_left')
                 elif hand.palm_velocity.y <= 10 and hand.palm_velocity.y >= -10:
                     if self._turning_direction(hand) == 'left':
