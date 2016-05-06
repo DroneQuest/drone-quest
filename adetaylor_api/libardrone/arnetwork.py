d67d5c754dc6681a1b9a143a84e5bae54c0ca9d2
Will Weatherford <weatherford.william@gmail.com>
2016-05-05 17:31:41 -0700

    Updated arnetwork to only inport arvideo instead of arvideo2 when appropriate.

--- adetaylor_api/libardrone/arnetwork.py
+++ adetaylor_api/libardrone/arnetwork.py
@@ -26,8 +26,6 @@ import threading
 import select
 import socket
 import struct
-# import multiprocessing
-from . import arvideo
 
 INIT_BYTES = b'\x01\x00\x00\x00'
 ARDRONE_IP = '192.168.1.1'
@@ -79,14 +77,18 @@ class ARDroneNetworkProcess(threading.Thread):
 
     def run(self):
 
+        from . import arvideo
+
         def _connect():
             logging.warn('Connection to ardrone')
             if self.is_ar_drone_2:
                 video_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                video_socket.connect(ARDRONE_VIDEO_ADDR)
+                video_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
                 video_socket.setblocking(0)
+                video_socket.connect(ARDRONE_VIDEO_ADDR)
             else:
                 video_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+                video_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
                 video_socket.setblocking(0)
                 video_socket.bind(('', ARDRONE_VIDEO_PORT))
                 video_socket.sendto(INIT_BYTES, ARDRONE_VIDEO_ADDR)
@@ -96,13 +98,15 @@ class ARDroneNetworkProcess(threading.Thread):
                 socket.SOCK_DGRAM,
                 socket.IPPROTO_UDP,
             )
+            nav_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
             nav_socket.setblocking(0)
             nav_socket.bind(('', ARDRONE_NAVDATA_PORT))
             nav_socket.sendto(INIT_BYTES, ARDRONE_NAVDATA_ADDR)
 
             control_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            control_socket.connect(ARDRONE_CONTROL_ADDR)
+            control_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
             control_socket.setblocking(0)
+            control_socket.connect(ARDRONE_CONTROL_ADDR)
             logging.warn('Connection established')
             return video_socket, nav_socket, control_socket
 
